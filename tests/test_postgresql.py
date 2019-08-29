114e22aec7ace1699c840961fb9c0026b41e5f2b
Paul Bryan <pbryan@anode.ca>
2019-08-28 19:09:04 -0700

    Add tests.

--- tests/test_postgresql.py
+++ tests/test_postgresql.py
@@ -8,40 +8,45 @@ from datetime import date, datetime
 from roax.resource import NotFound
 from uuid import uuid4
 
+import logging
+
+_logger = logging.getLogger(__name__)
 
 _schema = s.dict(
     {
-        "_id": s.uuid(),
-        "_dict": s.dict({"a": s.int()}),
-        "_list": s.list(s.int()),
-        "_set": s.set(s.str()),
-        "_int": s.int(),
-        "_float": s.float(),
-        "_bool": s.bool(),
-        "_bytes": s.bytes(format="binary"),
-        "_date": s.date(),
-        "_datetime": s.datetime(),
+        "id": s.uuid(),
+        "str": s.str(),
+        "dict": s.dict({"a": s.int()}),
+        "list": s.list(s.int()),
+        "set": s.set(s.str()),
+        "int": s.int(),
+        "float": s.float(),
+        "bool": s.bool(),
+        "bytes": s.bytes(format="binary"),
+        "date": s.date(),
+        "datetime": s.datetime(),
     }
 )
 
 
 @pytest.fixture(scope="module")
 def database():
-    db = postgresql.Database(minconn=1, maxconn=10, dbname="test")
+    db = postgresql.Database(minconn=1, maxconn=10, dbname="roax_postgresql")
     with db.cursor() as cursor:
         cursor.execute(
             """
                 CREATE TABLE FOO (
-                    _id text,
-                    _dict jsonb,
-                    _list jsonb,
-                    _set jsonb,
-                    _int integer,
-                    _float float,
-                    _bool boolean,
-                    _bytes bytea,
-                    _date date,
-                    _datetime timestamp
+                    id TEXT,
+                    str TEXT,
+                    dict JSONB,
+                    list JSONB,
+                    set JSONB,
+                    int INTEGER,
+                    float FLOAT,
+                    bool BOOLEAN,
+                    bytes BYTEA,
+                    date DATE,
+                    datetime TIMESTAMP WITH TIME ZONE
                 );
             """
         )
@@ -50,52 +55,58 @@ def database():
         cursor.execute("DROP TABLE FOO;")
 
 
-@pytest.fixture(scope="module")
-def table():
-    return postgresql.Table("foo", _schema, "_id")
+@pytest.fixture()
+def table(database):
+    with database.cursor() as cursor:
+        cursor.execute("DELETE FROM FOO;")
+    return postgresql.Table("foo", _schema, "id")
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def resource(database, table):
     return db.TableResource(database, table)
 
 
+import datetime
+
+
 def test_crud(resource):
     body = {
-        "_id": uuid4(),
-        "_dict": {"a": 1},
-        "_list": [1, 2, 3],
-        "_set": {"foo", "bar"},
-        "_int": 1,
-        "_float": 2.3,
-        "_bool": True,
-        "_bytes": b"12345",
-        "_date": s.date().str_decode("2019-01-01"),
-        #        "_datetime": s.datetime().str_decode("2019-01-01T01:01:01Z"),
+        "id": uuid4(),
+        "str": "string",
+        "dict": {"a": 1},
+        "list": [1, 2, 3],
+        "set": {"foo", "bar"},
+        "int": 1,
+        "float": 2.3,
+        "bool": True,
+        "bytes": b"12345",
+        "date": s.date().str_decode("2019-01-01"),
+        "datetime": s.datetime().str_decode("2019-01-01T01:01:01Z"),
     }
-    resource.create(body["_id"], body)
-    assert resource.read(body["_id"]) == body
-    body["_dict"] = {"a": 2}
-    body["_list"] = [2, 3, 4]
-    del body["_set"]
-    body["_int"] = 2
-    body["_float"] = 1.0
-    body["_bool"] = False
-    del body["_bytes"]
-    del body["_date"]
-    #   del body["_datetime"]
-    resource.update(body["_id"], body)
-    assert resource.read(body["_id"]) == body
-    resource.delete(body["_id"])
+    resource.create(body["id"], body)
+    assert resource.read(body["id"]) == body
+    body["dict"] = {"a": 2}
+    body["list"] = [2, 3, 4]
+    del body["set"]
+    body["int"] = 2
+    body["float"] = 1.0
+    body["bool"] = False
+    del body["bytes"]
+    del body["date"]
+    #   del body["datetime"]
+    resource.update(body["id"], body)
+    assert resource.read(body["id"]) == body
+    resource.delete(body["id"])
     with pytest.raises(NotFound):
-        resource.read(body["_id"])
+        resource.read(body["id"])
 
 
 def test_list(resource):
     count = 10
     for n in range(0, count):
         id = uuid4()
-        assert resource.create(id, {"_id": id}) == {"id": id}
+        assert resource.create(id, {"id": id}) == {"id": id}
     ids = resource.list()
     assert len(ids) == count
     for id in ids:
@@ -106,12 +117,11 @@ def test_list(resource):
 def test_list_where(resource):
     for n in range(0, 20):
         id = uuid4()
-        assert resource.create(id, {"_id": id, "_int": n}) == {"id": id}
+        assert resource.create(id, {"id": id, "int": n}) == {"id": id}
     where = resource.query()
-    where.text("_int < ")
-    where.param(resource.table.encode("_int", 10))
+    where.text("int < ")
+    where.param(resource.table.encode("int", 10))
     ids = resource.list(where=where)
-    print(f"ids = {ids}")
     assert len(ids) == 10
     for id in resource.list():
         resource.delete(id)
@@ -128,9 +138,31 @@ def test_rollback(resource):
     try:
         with resource.connect():  # transaction demarcation
             id = uuid4()
-            resource.create(id, {"_id": id})
+            resource.create(id, {"id": id})
             assert len(resource.list()) == 1
             raise RuntimeError  # force rollback
+    except RuntimeError:
+        pass
+    assert len(resource.list()) == 0
+
+
+def test_nested_connect(database):
+    with database.connect() as c1:
+        with database.connect() as c2:
+            assert c1 == c2
+        with database.connect() as c3:
+            assert c1 == c3
+
+
+def test_nested_connect_rollback(resource):
+    assert len(resource.list()) == 0
+    id = uuid4()
+    try:
+        with resource.database.connect() as c1:  # transaction demarcation
+            with resource.database.connect() as c2:
+                resource.create(id, {"id": id})
+                assert len(resource.list()) == 1
+                raise RuntimeError  # force rollback
     except RuntimeError:
         pass
     assert len(resource.list()) == 0
